aaa5c992410d63cef1024c02ba9493414d656da4
aichabbb <65061771+aichabbb@users.noreply.github.com>
2023-05-12 03:02:01 +0100

    Added new feature to file.py

---
+++ src/Cer.js
@@ -0,0 +1,22 @@
+import im from './cert.png';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+
+import React from 'react';
+
+function Cer() {
+  return (
+   <div className='container' style={{marginBottom:'10%'}}>
+    <div className='row' id='img_cer'>
+        <div className='col-sm'>
+             <img src={im} alt="Logo"  class="img-fluid"/>
+
+        </div>
+    </div>
+   </div>
+  );
+}
+
+export default Cer;
